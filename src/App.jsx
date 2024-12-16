47927998502324499111c934cbb5802d520d28cd
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-12-16 22:52:37 +0000

    Add files via upload
    Uploading source files.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./index.css";
+import Number from "./Number";
+
+function App() {
+  return (
+    <div className="App">
+      <Number number={1} text="one" />
+      <Number number={2} text="two" />
+      <Number number={3} text="three" />
+      <Number number={4} text="four" />
+      <Number number={5} text="five" />
+      <Number number={6} text="six" />
+      <Number number={7} text="seven" />
+      <Number number={8} text="eight" />
+      <Number number={9} text="nine" />
+      <Number number={10} text="ten" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Number.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Number = ({ number, text }) => {
+  return (
+    <div className="numberContainer">
+      <div className="container">
+        <h1>{number}</h1>
+        <h3>{text}</h3>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        doloribus eos fugiat veritatis quod magnam quas inventore, tempora
+        reprehenderit animi, debitis non. Nostrum facere unde nemo praesentium
+        ratione adipisci maiores. Repudiandae commodi, accusantium deserunt
+        numquam alias adipisci ullam, quod fugiat, soluta vitae accusamus hic
+        voluptate rerum quis asperiores nihil. Aperiam, quam obcaecati! Earum,
+        aut maxime. Accusantium molestiae vero odit deserunt? Aperiam assumenda
+        omnis, ipsum minus amet placeat reprehenderit laboriosam. Est, quam.
+        Obcaecati hic provident, suscipit nisi dolorum eligendi est ipsam sit
+        vero ut, totam possimus nam quod rerum quam excepturi. Asperiores sit
+        fugit cum repellendus dolor illum minus, mollitia minima autem alias.
+        Cupiditate nesciunt rem vero dolor cumque ex odit ea quis accusamus
+        velit, sapiente maiores enim! Excepturi, quam velit. Maxime architecto
+        cum consectetur aut exercitationem, omnis illum ipsa odio ea inventore
+        ipsam amet maiores voluptatem. Repudiandae, autem delectus doloremque
+        nisi assumenda corporis similique sint eum? Ipsum labore sunt commodi!
+        Sunt, iure libero! Eveniet placeat, soluta maxime voluptatum suscipit,
+        sequi officia, a recusandae fugiat cum eligendi illo dolor odit? Sed quo
+        minima alias hic sequi amet molestiae. Exercitationem, delectus libero!
+        Impedit, harum. Id consequatur eius cupiditate maiores, delectus qui
+        reprehenderit. Sequi laboriosam ad rem incidunt voluptatem, ipsam
+        tempore velit vero. Modi aspernatur dolor cupiditate explicabo quo.
+        Dicta adipisci dignissimos quo. Facere quae aliquid nobis eos minima
+        obcaecati magni omnis sunt hic deserunt molestiae, exercitationem, non,
+        natus impedit facilis. Facere dolore vel aut non, officia inventore
+        reiciendis magni perspiciatis omnis labore. In rerum, eveniet enim a
+        totam laboriosam! Ab similique excepturi ut consequatur iure saepe?
+        Sequi rem dicta rerum, ad laudantium accusantium quisquam amet obcaecati
+        fuga nihil voluptatibus! Quibusdam, autem nemo. Ex quisquam illo
+        praesentium? Exercitationem nulla animi libero optio illum repudiandae
+        corporis delectus, omnis in expedita sequi eveniet iste quod. Asperiores
+        corrupti earum hic eligendi deleniti deserunt. Id, molestiae. Vel?
+      </p>
+
+      <div className="circle"></div>
+    </div>
+  );
+};
+
+export default Number;
